c593639f03c7c230f9d50306f356c4fffa470c70
Abbah77 <yakubuyakson77@gmail.com>
2026-03-16 20:56:44 +0100

    Delete here_web directory

--- here_web/router.js
+++
@@ -1,6 +0,0 @@
-// Add these routes to router.js
-router.addRoute('/post/create', CreatePostPage, { requiresAuth: true });
-router.addRoute('/post/:id', PostDetailPage, { requiresAuth: true });
-router.addRoute('/profile/edit', ProfileEditPage, { requiresAuth: true });
-router.addRoute('/group/create', GroupChatPage, { requiresAuth: true });
-router.addRoute('/group/:id', GroupChatPage, { requiresAuth: true });
